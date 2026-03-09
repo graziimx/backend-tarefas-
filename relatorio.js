e46b4e8a669a70a15bd3a106188e8209a5cd91d4
graziimx <alexia.graziely.alves@escola.pr.gov.br>
2026-03-09 08:01:17 -0300

    Add files via upload

---
+++ relatorio.js
@@ -0,0 +1,13 @@
+//relatorio de notas
+console.log("relatorio de notas");
+
+let aluno=" Aalexia";
+let nota1 =8;
+let nota2 =7;
+let nota3 =4;
+let media = (nota1 + nota2 + nota3) / 3;
+ 
+//let relatorio = "Aluno": + aluno + "\n" +"notas:" + Nota1 + "," + Nota2 + "," + Nota3
+let relatorio = "Aluno:" + aluno + " | notas:" + nota1 + " e " + nota2 + " | media: " + media;
+
+console.log(relatorio);
