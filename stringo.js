bc9fb276f15cab39e2bde2b2077c6012763803de
graziimx <alexia.graziely.alves@escola.pr.gov.br>
2026-03-10 08:40:24 -0300

    Add script to log complete address

---
+++ stringo.js
@@ -0,0 +1,12 @@
+console.log("ENDEREÇO COMPLETO");
+
+let rua = "Rua das Flores";
+let numero = 9851;
+let cidade = "Curitiba";
+let cep = "80010-000";
+
+let enderecoCompleto = rua + "," + numero + "-" + cidade + "-" + cep;
+
+console.log(enderecoCompleto);
+
+//Adicione informacões adicionais
