9be50b50f4456442b4eeea5c1b628a11a03fad98
graziimx <alexia.graziely.alves@escola.pr.gov.br>
2026-03-11 09:44:31 -0300

    Add files via upload

--- hora.js
+++ hora.js
@@ -1,1 +1,12 @@
-// valores inicias do dia
+// Valores iniciais do dia
+let valorPorHora = 50;
+let horasTrabalhadas = 8; // Expediente padrão
+
+console.log("--- Resumo do Expediente ---");
+console.log(`Horas iniciais: ${horasTrabalhadas}h`);
+
+horasTrabalhadas++;
+console.log(`Após horas extra (horasTrabalhadas++): ${horasTrabalhadas}h`);
+
+horasTrabalhadas--;
+console.log(`Após saída antecipada: ${horasTrabalhadas}h`);
