4155e1ad2ba3375713434be42ab039a7c07bf496
graziimx <alexia.graziely.alves@escola.pr.gov.br>
2026-03-17 11:55:57 +0000

    versao2

--- hora.js
+++ hora.js
@@ -10,3 +10,10 @@ console.log(`Após horas extra (horasTrabalhadas++): ${horasTrabalhadas}h`);
 
 horasTrabalhadas--;
 console.log(`Após saída antecipada: ${horasTrabalhadas}h`);
+
+//Cálculo final
+let taxaSistema = 15;
+let pagamentoTotal = (horasTrabalhadas * valorPorHora) - taxaSistema;
+
+console.log("\n--- Fechamento ---");
+console.log(`Total a receber por $(horasTrabalhadas)H : $(pagamentoTotal)`);
