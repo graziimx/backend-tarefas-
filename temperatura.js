ddb2bc6545189bf339eb26e9c2a21a04c555af08
graziimx <alexia.graziely.alves@escola.pr.gov.br>
2026-03-23 13:47:34 +0000

    versao4

--- temperatura.js
+++ temperatura.js
@@ -3,11 +3,14 @@
 const cidade ="Curitiba";
 const tempCelsius = 18.5;
 
-const tempFahrenheit =temCelsiu * (9/5)+ 32;
-const tempKelvin = temCelsiu + 273.15;
-const variacaoDiaria = 26.3 - temCelsiu;
+const tempFahrenheit =tempCelsius * (9 / 5)+ 32;
+const tempKelvin = tempCelsius + 273.15;
+const variacaoDiaria = 26.3 - tempCelsius;
 
 console.log(`Temperatura em ${cidade} hoje está ${tempCelsius}.`);
 console.log(`Celsius: ${tempCelsius}`);
+console.log(`Fahrenheit: ${tempFahrenheit.toFixed(1)}Fº`);
+console.log(`Kelvin:${tempKelvin}Kº`);
+console.log(`\nVariação prevista: ${variacaoDiaria.toFixed(1)} Cº`);
 
-//Fazer console.log para Fahrenheit e para Kelvin
+//Tecla Alt Gr + tecla colchetes para fazer o
