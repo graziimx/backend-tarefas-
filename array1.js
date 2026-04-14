019fd381a846f7d57f2ff12e1cced554bfa1f09b
graziimx <alexia.graziely.alves@escola.pr.gov.br>
2026-04-14 11:39:48 +0000

    versao2

--- array1.js
+++ array1.js
@@ -1,13 +1,14 @@
 //Lista de frutas - ARRAY
 
-const frutas = ["Maçã", "Banana", "Uva", "Manga" ];
+const frutas = ["Maçã", "Banana", "abacate", "Manga" ];
 //ÌNDICE frutas 0 1 2 3
 
 console.log(frutas[1]);
 console.log(frutas[3]);
 console.log(frutas.length);
 frutas[1] = "Morango"; //Altera o valor do elemento
-frutas.push("Pessego");
+frutas.push("pessego");
+frutas.push("melão")
 frutas.unshift("Pera");
 console.log(frutas)
 console.log(frutas.length);
