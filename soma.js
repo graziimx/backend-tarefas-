1292e3a4414c7e34f7a6e2f23d4f6ffb91382b9d
graziimx <alexia.graziely.alves@escola.pr.gov.br>
2026-04-14 11:19:26 +0000

    versao1

---
+++ soma.js
@@ -0,0 +1,13 @@
+function somar(numero1, numero2) {
+    return numero1 + numero2
+}
+
+console.log(somar(5,2));
+
+function verificarPar(numero) {
+    if(numero % 2 === 0 ) {
+        return
+    }
+}
+
+console.log(verificarPar(8));
